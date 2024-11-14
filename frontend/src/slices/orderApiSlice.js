1a91799accf3f436f2dad0f82d18b12863c7f183
Ananth S <ananths@ideyalabs.com>
2024-11-14 18:09:07 +0530

    orderScreen-section10-checkout-process

--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -13,8 +13,14 @@ export const orderApiSlice = apiSlice.injectEndpoints({
           credentials: 'include'
         }
       }
+    }),
+    getOrderDetails: builder.query({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}`
+      }),
+      keepUnusedDataFor: 5
     })
   })
 });
 
-export const { useCreateOrderMutation } = orderApiSlice;
+export const { useCreateOrderMutation, useGetOrderDetailsMutation } = orderApiSlice;
