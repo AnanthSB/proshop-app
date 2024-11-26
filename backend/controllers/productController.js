5d02cb9cec278d60298c028b2c2d5e8251b07236
Ananth S <ananths@ideyalabs.com>
2024-11-26 18:22:19 +0530

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -42,4 +42,12 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct);
 });
 
-export { getProducts, getProductById, createProduct };
+// desc   Fetch all products
+//@route  GET /api/products
+//access  Public
+const updateProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+export { getProducts, getProductById, createProduct, updateProducts };
